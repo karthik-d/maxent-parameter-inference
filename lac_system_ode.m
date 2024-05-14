6ff127aa1b3fb8c63cf12d9fa0d93f170d11f6e6
Karthik Desingu <karthikdesingu2000@gmail.com>
2024-05-14 03:13:15 -0400

    TMG nullcline

--- lac_system_ode.m
+++ lac_system_ode.m
@@ -1,5 +1,6 @@
 function funcs = lac_system_ode
     funcs.dydt = @lac_system_dydt;
+    funcs.nullclines = @lac_system_nullclines;
 end
 
 
@@ -32,8 +33,39 @@ function dydt = lac_system_dydt(t, y, consts, params)
 end
 
 
-function exprs = lac_system_exprs
+function [tmgi_null, lacy_null] = lac_system_nullclines(consts, params)
+    syms tmg_i lac_y alph beta_t beta_g r r0 rT rate n;
+    
+    % set up helpers.
+	null_eqn = rate == 0;
+	exprs = lac_system_exprs;
 
+    % solve dependent parameters and consts.
+    consts.r0 = consts.rT/(params.rho - 1);
+    params.alph = 84.4/(1 + (consts.glu/8.1)^1.2) + 16.1;
+    params.beta_t = 1.23e-03 * consts.tmg_e^0.6;
+    
+    % tmg_i nullcline.
+    tmgi_eqn = compose(null_eqn, exprs.tmg_i);
+    tmgi_eqn = subs(tmgi_eqn, ...
+        {alph beta_t beta_g r0 rT params.n}, ...
+        {params.alph params.beta_t params.beta_g consts.r0 consts.rT params.n} ...
+    );
+	tmgi_null = solve(tmgi_eqn, tmg_i);
+
+    % % lacy nullcline.
+    % lacy_eqn = compose(null_eqn, exprs.lac_y);
+    % lacy_eqn = subs(lacy_eqn, ...
+    %     {alph beta_t beta_g r r0 rT params.n}, ...
+    %     {params.alph params.beta_t params.beta_g lac_i consts.r0 consts.rT params.n} ...
+    % );
+	% lacy_null = solve(lacy_eqn, lac_y);
+    lacy_null = 0;
+
+end
+
+
+function exprs = lac_system_exprs
     syms tmg_i lac_y alph beta_t beta_g r r0 rT n;
 
     exprs.tmg_i = beta_t*beta_g*lac_y - tmg_i;
