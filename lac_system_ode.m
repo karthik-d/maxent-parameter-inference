20c62b2787138bd104009247719d309937a8178f
Karthik Desingu <karthikdesingu2000@gmail.com>
2024-05-14 02:53:09 -0400

    numeric soln for ODE

--- lac_system_ode.m
+++ lac_system_ode.m
@@ -9,7 +9,7 @@ function dydt = lac_system_dydt(t, y, consts, params)
     exprs = lac_system_exprs;
 
     % compute dependent params and constants.
-    consts.rT = consts.r0*(params.rho - 1);
+    consts.r0 = consts.rT/(params.rho - 1);
     params.alph = 84.4/(1 + (consts.glu/8.1)^1.2) + 16.1;
     params.beta_t = 1.23e-03 * consts.tmg_e^0.6;
 
@@ -20,14 +20,15 @@ function dydt = lac_system_dydt(t, y, consts, params)
     
     % dydt is a vector of rate eqns: [tmg_i; lac_y].
 
-    dydt(1) = double(subs(exprs.tmg_i, ...
+    dydt = [...
+        double(subs(exprs.tmg_i, ...
         {lac_y tmg_i alph beta_t beta_g r r0 rT params.n}, ...
-        {y(2) y(1) params.alph params.beta_t params.beta_g lac_i consts.r0 consts.rT params.n} ...
-    ));
-    dydt(2) = double(subs(exprs.lac_y, ...
+        {y(2) y(1) params.alph params.beta_t params.beta_g lac_i consts.r0 consts.rT params.n})) ...
+    ; ...
+        double(subs(exprs.lac_y, ...
         {lac_y tmg_i alph beta_t beta_g r r0 rT params.n}, ...
-        {y(2) y(1) params.alph params.beta_t params.beta_g lac_i consts.r0 consts.rT params.n} ...
-    ));
+        {y(2) y(1) params.alph params.beta_t params.beta_g lac_i consts.r0 consts.rT params.n})) ...
+    ];
 end
 
 
@@ -35,7 +36,7 @@ function exprs = lac_system_exprs
 
     syms tmg_i lac_y alph beta_t beta_g r r0 rT n;
 
-    exprs.tmg_i = beta_t*beta_g*lac_y - tmg_i
-    exprs.lac_y = alph*(r0/(r + r0)) - lac_y
-    exprs.lac_i = rT*(1/(1 + tmg_i^n))
+    exprs.tmg_i = beta_t*beta_g*lac_y - tmg_i;
+    exprs.lac_y = alph*(r0/(r + r0)) - lac_y;
+    exprs.lac_i = rT*(1/(1 + tmg_i^n));
 end
