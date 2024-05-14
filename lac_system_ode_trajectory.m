88f09cf24863f831e52b22a2e0757b4e7bffcb32
Karthik Desingu <karthikdesingu2000@gmail.com>
2024-05-14 07:49:46 -0400

    ode solver parameterized

--- lac_system_ode_trajectory.m
+++ lac_system_ode_trajectory.m
@@ -1,32 +1,29 @@
-function x = lac_system_ode_solve
+function trajectory = lac_system_ode_trajectory(rho, beta_g, n, lacy_init)
 
 	consts.glu = 50;
 	consts.tmg_e = 80;
-
 	% total LacI conc.
 	consts.rT = 0.4e-3;
 
 	% model params.
-	params.rho = 167.1;
-	params.beta_g = 65;
+	params.rho = rho;
+	params.beta_g = beta_g;
 	params.n = 2;
 
 	% time duration.
 	consts.tmax = 50;
-
 	% get ODE functions.
 	ode_funcs = lac_system_ode;
-
-	figure; hold on; zoom on;
 	% initial concentrations.
-	consts.y_init = [0; 8];
+	consts.y_init = [0; lacy_init];
 
 	% solve ODE.
 	disp("solving ode");
 	[ode_tout, x] = ode45(@(t,y) ode_funcs.dydt(t, y, consts, params), [0; consts.tmax], consts.y_init);
-	disp("ode solved");
+	% disp("ode solved");
 
+	% package for exporting to python.
 	ode_lacy = x(:, 2);
-	trajectory = cat(2, ode_tout, ode_lacy)
+	trajectory = cat(2, ode_tout, ode_lacy);
 
 end
--- run_ode.py
+++ run_ode.py
@@ -1,5 +1,10 @@
 import matlab.engine
+# import numpy as np
+
+rho = 167.1
+beta_g = 65.0
+n = 2.0
+lacy_init = 36.0
 
 eng = matlab.engine.start_matlab()
-val = eng.lac_system_ode_solve()
-print(val)
+val = eng.lac_system_ode_trajectory(rho, beta_g, n, lacy_init)
