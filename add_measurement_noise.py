c208786aa81ff0d5cb41bb22308cf851a3915bfe
Karthik Desingu <karthikdesingu2000@gmail.com>
2024-05-14 08:19:51 -0400

    export simulated data; bin time

--- add_measurement_noise.py
+++ add_measurement_noise.py
@@ -3,9 +3,21 @@ import numpy as np
 import os 
 
 
-DATA_FILE = os.path.join("./data/")
+DATA_FILE = os.path.join("./data/lacy_at_init_36.csv")
+
+
+def bin_timepoints(trajectory):
+	trajectory = np.array(trajectory)
+	trajectory_binned = np.array([[
+		10*i, 
+		trajectory[np.argmin(np.abs(trajectory[:, 0] - 10*i)), 1]
+		] for i in range(6)
+	])
+	return trajectory_binned
+
 
 # first column is the time step.
 # each row corresponds to specific time point. 
-# each column is a measurement of a variable for a specific ecoli, formatted as var_n.
-data_df = pd.read_csv()
+data_df = pd.read_csv(DATA_FILE, index_col=None, header=None)
+trajectory = bin_timepoints(data_df.to_numpy()) 
+print(trajectory)
