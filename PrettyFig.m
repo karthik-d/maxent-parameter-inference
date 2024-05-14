2be18559c6885971ad74cf8f9e3c75225d1b24a8
Karthik Desingu <karthikdesingu2000@gmail.com>
2024-05-14 16:15:55 -0400

    cleanup

--- PrettyFig.m
+++ PrettyFig.m
@@ -6,11 +6,6 @@
 % 4. makes all plots thicker 
 % 5. (optional) equalises Y axis in all subplots. to use: PrettyFig('EqualiseY=1;')
 % 6. makes figure background white
-% 
-% created by Srinivas Gorur-Shandilya at 10:20 , 09 April 2014. Contact me at http://srinivas.gs/contact/
-% 
-% This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
-% To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 
 function [] = PrettyFig(varargin)
 
