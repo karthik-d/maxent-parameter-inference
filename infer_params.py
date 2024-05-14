21ea7662fb55846ce3e3229e14a190c9bac6a73c
Karthik Desingu <karthikdesingu2000@gmail.com>
2024-05-14 09:40:17 -0400

    generate silico data

--- infer_params.py
+++ infer_params.py
@@ -1,10 +1,37 @@
+import matlab.engine
 import pandas as pd
 import numpy as np
 import maxent
 import os
 
+from .prepare_data import get_timepoint_data, bin_timepoints
 
-DATA_FILE = os.path.join("./data/")
+DATA_FILE = os.path.join("./data/lacy_at_init_36.csv")
+
+"""
+rho = 167.1
+beta_g = 65.0
+n = 2.0
+lacy_init = 36.0
+"""
+
+def restraint(rho, beta_g, n, lacy_init, t):
+	predicted_traj = eng.lac_system_ode_trajectory(rho, beta_g, n, lacy_init)
+	predicted_traj = bin_timepoints(predicted_traj)
 
 
-data_df = pd.read_csv(DATA_FILE)
+# start matlab engine.
+eng = matlab.engine.start_matlab()
+
+# get and prepare observation data.
+obs_trajectory = get_binned_timepoint_data(DATA_FILE)
+
+# prepare MaxEnt restraint per timepoint.
+restraints_l = [
+	maxent.Restraint(lambda traj: traj[-1,0], target=2)
+]
+rx = 
+ry = maxent.Restraint(lambda traj: traj[-1,1], target=1)
+
+# create model by passing in restraints.
+model = maxent.MaxentModel([rx, ry])
---
+++ prepare_data.py
@@ -0,0 +1,37 @@
+import pandas as pd 
+import numpy as np 
+import os 
+
+IN_FILE = os.path.join("./data/lacy_at_init_36.csv")
+OUT_FILE = os.path.join("./data/lacy_at_init_36_gen.csv")
+
+
+def bin_timepoints(trajectory):
+	trajectory = np.array(trajectory)
+	trajectory_binned = np.array([[
+		10*i, 
+		trajectory[np.argmin(np.abs(trajectory[:, 0] - 10*i)), 1]
+		] for i in range(6)
+	])
+	return trajectory_binned
+
+
+def get_binned_timepoint_data(filepath):
+	# first column is the time step.
+	# each row corresponds to specific time point. 
+	data_df = pd.read_csv(filepath, index_col=None, header=None)
+	trajectory = bin_timepoints(data_df.to_numpy()) 
+	return trajectory
+
+
+def add_noise_make_data(ref_trajectory, n_ecoli=1000):
+
+	# ref_trajectory should be a n_timepoints x 2 matrix: time, value.
+	# add standard normal noise to reference trajectory to generate synthetic data. 
+	noise_matrix = np.random.normal(0, 1, size=(n_ecoli, ref_trajectory.shape[0]))
+	data_matrix = np.array([ref_trajectory[:, 1] for _ in range(n_ecoli)]) + noise_matrix 
+	pd.DataFrame(data_matrix).to_csv(OUT_FILE, index=False, header=False)
+
+
+add_noise_make_data(get_binned_timepoint_data(IN_FILE))
+	
