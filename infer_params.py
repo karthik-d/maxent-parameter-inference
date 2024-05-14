ed4f177709b193bbab8bad626f383bd8982236f6
Karthik Desingu <karthikdesingu2000@gmail.com>
2024-05-14 09:58:13 -0400

    maxent inference

--- infer_params.py
+++ infer_params.py
@@ -1,12 +1,14 @@
 import matlab.engine
+import tensorflow as tf
 import pandas as pd
 import numpy as np
 import maxent
 import os
 
-from .prepare_data import get_timepoint_data, bin_timepoints
+from prepare_data import get_binned_timepoint_data, bin_timepoints
 
-DATA_FILE = os.path.join("./data/lacy_at_init_36.csv")
+REF_FILE = os.path.join("./data/lacy_at_init_36.csv")
+DATA_FILE = os.path.join("./data/lacy_at_init_36_gen.csv")
 
 """
 rho = 167.1
@@ -15,23 +17,25 @@ n = 2.0
 lacy_init = 36.0
 """
 
-def restraint(rho, beta_g, n, lacy_init, t):
-	predicted_traj = eng.lac_system_ode_trajectory(rho, beta_g, n, lacy_init)
-	predicted_traj = bin_timepoints(predicted_traj)
-
-
 # start matlab engine.
-eng = matlab.engine.start_matlab()
+# eng = matlab.engine.start_matlab()
 
 # get and prepare observation data.
-obs_trajectory = get_binned_timepoint_data(DATA_FILE)
+ref_trajectory = get_binned_timepoint_data(REF_FILE)[:, 0]
+obs_data = pd.read_csv(DATA_FILE, header=None).to_numpy()
 
 # prepare MaxEnt restraint per timepoint.
 restraints_l = [
-	maxent.Restraint(lambda traj: traj[-1,0], target=2)
+	maxent.Restraint(lambda traj, idx=idx: traj[idx], target=ref_trajectory[idx])
+	for idx in range(len(ref_trajectory))
 ]
-rx = 
-ry = maxent.Restraint(lambda traj: traj[-1,1], target=1)
 
 # create model by passing in restraints.
-model = maxent.MaxentModel([rx, ry])
+model = maxent.MaxentModel(restraints_l)
+model.compile(tf.keras.optimizers.Adam(1e-4), "mean_squared_error")
+# short burn-in
+h = model.fit(obs_data, epochs=500, steps_per_epoch=1000/32)
+# maxent weights.
+maxent_weights = model.traj_weights
+np.savetxt("maxent_traj_weights.txt", maxent_weights)
+
