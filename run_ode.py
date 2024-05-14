d24952fbf463b385795c9980005680713a2ed82a
Karthik Desingu <karthikdesingu2000@gmail.com>
2024-05-14 08:11:37 -0400

    timepoint binning

--- run_ode.py
+++ run_ode.py
@@ -1,10 +1,24 @@
 import matlab.engine
-# import numpy as np
+import numpy as np
 
 rho = 167.1
 beta_g = 65.0
 n = 2.0
 lacy_init = 36.0
 
+
 eng = matlab.engine.start_matlab()
-val = eng.lac_system_ode_trajectory(rho, beta_g, n, lacy_init)
+trajectory = eng.lac_system_ode_trajectory(rho, beta_g, n, lacy_init)
+
+
+# extract 6 time points only: 0, 10, ..., 50.
+def bin_timepoints(trajectory):
+	trajectory = np.array(trajectory)
+	print(trajectory.shape)
+
+	trajectory_binned = np.array([[
+		10*i, 
+		trajectory[np.argmin(np.abs(trajectory[:, 0] - 10*i)), 1]
+		] for i in range(6)
+	])
+	return trajectory_binned
